647b06c3ad63632517020914b11fccb071f1d2ac
Valerius Mattfeld <10323945+valerius21@users.noreply.github.com>
2020-06-25 22:50:52 +0200

    added telegram logo to footernav

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -8,6 +8,23 @@ const FooterSocial = ({ className, ...props }) => {
     <div {...props} className={classes}>
       <ul className="list-reset">
         <li>
+          <p style={{ display: "none" }}>
+            Icon from
+            https://www.flaticon.com/de/kostenloses-icon/telegramm_1216847
+          </p>
+          <a href="#">
+            <svg
+              height="16pt"
+              viewBox="0 -24 512 512"
+              width="16pt"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="m512 0-512 217.628906 134.222656 53.269532 47.78125 179.605468 87.980469-85.246094 143.132813 98.855469zm-323.023438 293.921875-5.167968 46.828125-19.839844-74.589844 186.898438-127.296875zm168.003907-195.472656-212.082031 144.449219-66.070313-26.21875zm-147.609375 283.816406 6.039062-54.703125 29.433594 20.332031zm17.527344-83.179687 239.648437-229.535157-73.371094 344.375zm0 0" />
+              <title>Telegram Channel</title>
+            </svg>
+          </a>
+        </li>
+        <li>
           <a href="https://www.facebook.com/fginfogoe/">
             <svg
               width="16"
